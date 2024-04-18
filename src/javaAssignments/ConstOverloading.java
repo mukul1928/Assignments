17513220b07f71e1f51335f93188fb42ec3ac190
JayashreeSureban14 <jaya.sureban@gmail.com>
2024-04-18 10:00:15 +0530

    update on 18/4/2024

--- src/javaAssignments/ConstOverloading.java
+++ src/javaAssignments/ConstOverloading.java
@@ -82,5 +82,5 @@ public class ConstOverloading {
 		System.out.println("Section: "+section);
 		System.out.println("City: "+city);		
 		
-	}
+	} 
 }
---
+++ src/javaAssignments/Demo.java
@@ -0,0 +1,18 @@
+package javaAssignments;
+
+public class Demo {
+	
+	static void add()
+	{
+		int a=10;
+		System.out.println("addition");
+		System.out.println(a+a);
+	}
+
+	public static void main(String[] args) {
+		
+		add();
+
+	}
+
+}
