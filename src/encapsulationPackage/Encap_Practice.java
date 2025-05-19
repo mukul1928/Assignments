51c88bde00918a7d8115d24f3db6786a7f4a9c93
Chandan Pandey <chandanpandey1928@gmail.com>
2025-05-19 13:11:47 +0530

    assignments

--- src/encapsulationPackage/Encap_Practice.java
+++ src/encapsulationPackage/Encap_Practice.java
@@ -1,36 +1,38 @@
 package encapsulationPackage;
 
-class Sensitive
-{
-	private String username = "jaya";
+class Sensitive {
+	private String username = "mukulpandey95";
 	private String password = "As@3*00";
-	
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
-	}	
-	
+	}
+
 }
 
 public class Encap_Practice {
 
 	public static void main(String[] args) {
-		
-		Sensitive s1=new Sensitive();
-		s1.setUsername("JayashreeMS");
+
+		Sensitive s1 = new Sensitive();
+		s1.setUsername("MukulPandey1928@");
 		System.out.println(s1.getUsername());
-		
+
 		s1.setPassword("As%%%000");
 		System.out.println(s1.getPassword());
-		
+
 	}
 
 }
